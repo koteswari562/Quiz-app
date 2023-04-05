382f40c65317698c3dc25e25a758a57477b322d4
Koteswari Ganta <118552078+koteswari562@users.noreply.github.com>
2023-04-05 14:44:59 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,169 @@
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+
+const quizArray = [
+  {
+    id: "0",
+    question: "HTML stands for",
+    options: ["Hyper Text Markup Language", "Hyper Text Makeup Language", "Hypo Text Markup Language", "Hypo Transfer Makeup Language"],
+    correct: "Hyper Text Markup Language",
+  },
+  {
+    id: "1",
+    question: "CSS stands for",
+    options: ["Cascading Style Sheet", "Comma Style Sheet", "Cascading Structure Sheet", "Cascading Style Structure"],
+    correct: "Cascading Style Sheet",
+  },
+  {
+    id: "2",
+    question: "Which is not a type of CSS?",
+    options: ["Inline", "Internal", "External", "Outline"],
+    correct: "Outline",
+  },
+];
+
+//Next Button
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    //increment questionCount
+    questionCount += 1;
+    //if last question
+    if (questionCount == quizArray.length) 
+    {
+      //hide question container and display score
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      document.body.style.backgroundColor="white";
+      //user score
+      if(scoreCount>=2){
+        const img = document.createElement("img");
+        img.src = "pass.gif";
+        img.style.margin="2% 37%";
+        document.body.appendChild(img);
+      }
+      else
+      {
+        const img = document.createElement("img");
+        img.src = "fail.gif";
+        img.style.width="24%"
+        img.style.margin="2% 37%";
+        document.body.appendChild(img);
+      }
+      userScore.innerHTML ="Your score is " + scoreCount + "/" + questionCount;
+    } else 
+    {
+      //display questionCount
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      //display quiz
+      quizDisplay(questionCount);
+      count = 11;
+      //clearInterval(countdown);
+      //timerDisplay();
+    }
+  })
+);
+
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  //Hide other cards
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  //display current question card
+  quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation
+function quizCreator() {
+  //randomly sort questions
+  quizArray.sort(() => Math.random() - 0.5);
+  //generate quiz
+  for (let i of quizArray) {
+    //randomly sort options
+    i.options.sort(() => Math.random() - 0.5);
+    //quiz card creation
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    //question number
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    //question
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    //options
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
+}
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
+
+  //if user clicked answer == correct option stored in object
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    //For marking the correct option
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  //disable all options
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+//initial setup
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 11;
+  //clearInterval(countdown);
+  //timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+//when user click on start button
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
+
+//hide quiz and display start screen
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
